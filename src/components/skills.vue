60c0d324a42c40f86fa717f808aefac37d675f55
住江 祐哉 <yuya@sumienoMacBook-puro.local>
2019-04-26 16:29:06 +0900

    add computerVision link

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -3,7 +3,8 @@
     <h1>Skills</h1>
     <div class="uk-child-width-expand@s uk-text-center" uk-grid style="position:inherit">
       <div>
-        <div class="uk-card uk-card-default uk-card-body">コンピュータビジョン</div>
+        <div class="uk-card uk-card-default uk-card-body">
+          <a href="https://www.slideshare.net/secret/upZ841uIHPje3k">コンピュータビジョン</a></div>
       </div>
       <div>
         <div class="uk-card uk-card-default uk-card-body">UI作成</div>
